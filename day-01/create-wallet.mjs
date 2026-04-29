7e67269474736a4fd38d3c79dc9cd6a501f0cb11
Siddhant0122 <siddhant.chavan126@gmail.com>
2026-04-30 01:37:32 +0530

    Day-1: Learned how solana address works and  which algorithm is used to generate keys, also learned how developer test to create production level project.

---
+++ day-01/create-wallet.mjs
@@ -0,0 +1,26 @@
+import {generateKeyPairSigner, 
+    createSolanaRpc,
+    devnet,
+ } from "@solana/kit";
+
+const rpc = createSolanaRpc(devnet("https://api.devnet.solana.com"));
+// const wallet = await generateKeyPairSigner(); //generates a address
+
+// console.log("Wallet address: ", wallet.address); //address of wallet created
+// console.log("\n--- Go to https://faucet.solana.com/ and airdrop SOL to this address ---");
+// console.log("--- Then run this script again with the same address to check the balance ---\n");
+
+const wallet = "GB5tFWAq5Lny2Atp7utJmNS4jvLhoxng9CZpHwMfai5z";
+console.log("Wallet address: ", wallet);
+const {value: balance} = await rpc.getBalance(wallet).send();
+const balanceInSol = Number(balance) / 1_000_000_000;
+
+console.log(`Balance ${balanceInSol} SOL`)
+
+//console.log("Your new wallet address:", wallet)
+//console.log(
+//  "\nThis address is your public key. It's safe to share."
+//);
+// console.log(
+//   "The private key stays in memory. In a real app, you'd save it securely."
+// ); 

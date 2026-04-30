f31249f8c5c181ab7ce34aba7f0377093cd5d3bd
Siddhant0122 <siddhant.chavan126@gmail.com>
2026-04-30 22:32:47 +0530

    Day 1 of address and airdropping it in solana

--- day-01/create-wallet.mjs
+++ day-01/create-wallet.mjs
@@ -10,7 +10,7 @@ const rpc = createSolanaRpc(devnet("https://api.devnet.solana.com"));
 // console.log("\n--- Go to https://faucet.solana.com/ and airdrop SOL to this address ---");
 // console.log("--- Then run this script again with the same address to check the balance ---\n");
 
-const wallet = "GB5tFWAq5Lny2Atp7utJmNS4jvLhoxng9CZpHwMfai5z";
+const wallet = "GB5tFWAq5Lny2Atp7utJmNS4jvLhoxng9CZpHwMfai5z";//randomly generated address from above code snippet, you can use your own address here to check balance after airdropping SOL to it
 console.log("Wallet address: ", wallet);
 const {value: balance} = await rpc.getBalance(wallet).send();
 const balanceInSol = Number(balance) / 1_000_000_000;
